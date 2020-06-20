b035bfea45e07bd07e2624a2411b07eb4212580c
marekksiazek <marek.ksiazek1990@gmail.com>
2020-06-20 16:26:00 +0200

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('quiz', { title: 'Quiz' });
+  const show = !req.session.vote
+
+  Quiz.find({}, (err, data) => {
+    let sum = 0;
+    data.forEach((item) => {
+      sum += item.vote;
+    })  
+
+    res.render('quiz', { title: 'Quiz', data, show, sum });
+  });
+  
+});
+
+
+router.post('/', (req, res, next) => {
+  const body = req.body.quiz;
+
+  Quiz.findOne({_id: body}, (err, data) => {
+    data.vote++;
+    data.save((err) => {
+      req.session.vote = 1;
+      res.redirect('/quiz');
+    });
+  });
 });
 
 module.exports = router;
